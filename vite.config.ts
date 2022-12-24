f88a384dd6f62f1954418c79972e851b42ced927
Filip Wachowiak <wachowiakf@gmail.com>
2022-12-24 23:16:25 +0100

    Fix vitest exclusion

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
-import solid from 'solid-start/vite'
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
+import solid from 'solid-start/vite'
 import dotenv from 'dotenv'
 
 dotenv.config()
@@ -7,4 +8,13 @@ dotenv.config()
 export default defineConfig({
   plugins: [solid()],
   ssr: { external: ['@prisma/client'] },
+  test: {
+    exclude: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/e2e/**',
+      '**/.{idea,git,cache,output,temp}/**',
+      '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress}.config.*',
+    ],
+  },
 })
